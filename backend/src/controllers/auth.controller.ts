3d9c1ab4f85a72e0a8eb930d443485b23dddf66e
Pito Desri Pauzi <pitokfauzi@gmail.com>
2025-05-08 23:09:52 +0700

    kalau ini

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -20,9 +20,8 @@ export async function login(req: Request, res: Response) {
         res.cookie('token', token, {
             httpOnly: true,
             secure: true,
-            sameSite: "strict",
+            sameSite: "none",
             maxAge: 30 * 24 * 60 * 60 * 1000, // 30 hari
-            path: '/',
         });
 
 
